b2b96b2306088b202d410416ad04fe648130cf53
Feyaaz <feyaazc94@gmail.com>
2024-07-22 22:46:17 +0100

    Add Pytest Files

---
+++ tests/test_movie_routes.py
@@ -0,0 +1,32 @@
+import pytest
+from fastapi import HTTPException
+
+
+class TestMovie:
+    def test_get_movies_with_no_movies_in_database(self, client):
+        response = client.get("/movies")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "Not Found: Unable to find movie in Database"}
+
+    def test_get_movies_with_movies_in_database(self, db_session, client, create_mock_movie):
+        db_session.add(create_mock_movie)
+        db_session.commit()
+
+        response = client.get("/movies")
+        print(response.json())
+        assert response.status_code == 200
+        assert response.json() == {
+            "message": "Movie data retrieved from database",
+            "data": [
+                {
+                    "id": 1,
+                    "user_id": None,
+                    "title": "Test Movie",
+                    "genre": "Test Genre",
+                    "year": "2021",
+                    "runtime": "120 min",
+                    "rating": 5,
+                    "avr_rating": 5,
+                }
+            ],
+        }
