56547e83e6157d72ad60ede686f2822b551290af
Feyaaz <feyaazc94@gmail.com>
2024-07-23 17:00:03 +0100

    Add New Tests

--- tests/test_movie_routes.py
+++ tests/test_movie_routes.py
@@ -1,4 +1,9 @@
-class TestMovie:
+from unittest.mock import patch
+
+from sqlalchemy.exc import OperationalError, ProgrammingError
+
+
+class TestMovieRoutes:
     def test_get_movies_with_no_movies_in_database(self, client):
         response = client.get("/movies")
         assert response.status_code == 404
@@ -25,3 +30,19 @@ class TestMovie:
                 }
             ],
         }
+
+    @patch("app.methods.sql_alchemy_crud.get_movies_info")
+    def test_get_movies_with_bad_request(self, mock_get_movies_info, client):
+        mock_get_movies_info.side_effect = OperationalError("Mock Operational Error", "", "")
+        response = client.get("/movies")
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Bad request: Connection to Database could not be established"
+        }
+
+    @patch("app.methods.sql_alchemy_crud.get_movies_info")
+    def test_get_movies_with_no_movies_table_found(self, mock_get_movies_info, client):
+        mock_get_movies_info.side_effect = ProgrammingError("Mock Programming Error", "", "")
+        response = client.get("/movies")
+        assert response.status_code == 400
+        assert response.json() == {"detail": "Bad request: Movies table does not exist in Database"}
