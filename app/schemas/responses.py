81d0b5b70f78830b77cbd5d63ce5adeb9ac8704d
Feyaaz <feyaazc94@gmail.com>
2024-07-25 23:23:48 +0100

    Add Examples To Schema

--- app/schemas/responses.py
+++ app/schemas/responses.py
@@ -1,18 +1,47 @@
-from typing import Any, Dict, List, Optional
+from typing import List, Optional
 
 from pydantic import BaseModel
+
+from app.schemas.base import EXAMPLE_JSON, MovieSchema
 
 
 class MovieResponse(BaseModel):
     message: str
-    data: Optional[List[Dict]] = None
+    data: Optional[List[MovieSchema]] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": "Movie data retrieved from database",
+                "data": [EXAMPLE_JSON],
+            }
+        }
+    }
 
 
 class RatingResponse(BaseModel):
     message: str
-    data: Optional[List[Dict]] = None
+    data: Optional[List[MovieSchema]] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": f"Top five average rated movies for {EXAMPLE_JSON['user_id']} retrieved from database",
+                "data": [EXAMPLE_JSON],
+            }
+        }
+    }
 
 
 class UpdateRatingResponse(BaseModel):
     message: str
-    data: Dict[str, Any]
+    data: MovieSchema
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "message": f"Rating value has changed for USER-ID: {EXAMPLE_JSON['user_id']} and MOVIE-ID: {EXAMPLE_JSON['id']}",
+                "data": [EXAMPLE_JSON],
+            }
+        }
+    }
